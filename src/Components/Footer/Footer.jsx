93604a6bc7688fbbdc50eba8428bdadbb3820be4
Sarafat2002 <sarafathossen58@gmail.com>
2026-05-19 10:46:17 +0600

    map index problem solved

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+        <div>
+            <h1>DigiTools</h1>
+            <p>Premium digital tools for creators,<br/>professional and businesses.Work smarter<br/>with our suite of powerful tools</p>
+        </div>
+        <div>
+            
+        </div>
+    </div>
   )
 }
 
